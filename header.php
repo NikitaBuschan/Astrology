4760db4fca9c4cfcae897807da13ec6dc0819172
Nikita <nikitabushhan@gmail.com>
2021-05-12 09:50:05 +0300

    create header and make responsive with burger

--- header.php
+++ header.php
@@ -13,5 +13,44 @@
 <body>
     <div class="wrapper">
         <header class="header">
+            <div class="header__container">
+                <div class="header__body">
+                    <a href="#" class="header__logo__with__burger">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png">
+                    </a>
+                    <div class="header__burger">
+                        <span>
 
+                        </span>
+                    </div>
+                    <nav class="header__menu">
+                        <ul class="header__list">
+                            <li>
+                                <a href="/index.php" class="header__link">Главная</a>
+                            </li>
+                            <li>
+                                <a href="/programs" class="header__link">Программы</a>
+                            </li>
+                            <li>
+                                <a href="/services" class="header__link" >Услуги и цены</a>
+                            </li>
+                            <li>
+                                <a href="#" class="header__logo">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="header__link">Специалисты</a>
+                            </li>
+                            <li>
+                                <a href="#" class="header__link">Исследования</a>
+                            </li>
+                            <li>
+                                <a href="/contacts" class="header__link">Контакты</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <script src="<?php echo get_template_directory_uri(); ?>/scripts/header.js"></script>
         </header>
